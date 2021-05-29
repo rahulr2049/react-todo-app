ee2c34a3e706ccf873110c31f1f2a6009a5120ac
rahulr2049 <rahulr2049@gmail.com>
2021-05-29 11:32:46 +0530

    Added bootstrap css  and react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Todo from "./Todo";
+import FormTodo from "./FormTodo";
+import { Card } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css"
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const markTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isDone = !newTodos[index].isDone;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text, isDone: false }];
+    setTodos(newTodos);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h1>Todo List</h1>
+        <FormTodo addTodo={addTodo} />
+        <Card>
+          {todos.map((todo, index) => (
+            <Card.Body>
+              <Todo
+                key={index}
+                index={index}
+                todo={todo}
+                markTodo={markTodo}
+                removeTodo={removeTodo}
+              />
+            </Card.Body>
+          ))}
+        </Card>
+      </div>
     </div>
   );
 }
---
+++ src/Todo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Todo = ({ todo, index, markTodo, removeTodo }) => {
+    return (
+        <div>
+            <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>{todo.text}</span>
+            <div>
+                <button variant="outline-success" onClick={() => markTodo(index)}>✓</button>{' '}
+                <button variant="outline-danger" onClick={() => removeTodo(index)}>✕</button>
+            </div>
+        </div>
+    )
+}
+
+export default Todo;
